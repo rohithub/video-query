99a74bea161e9cd3204f6f6cc0ff48a2e54d3473
rohitkum <rohitkum@umich.edu>
2017-10-30 19:47:38 -0400

    Version 2, waitkey = 5ms, Resize to 800x600, RGB to Gray

--- sys_perf/captureVid.cpp
+++ sys_perf/captureVid.cpp
@@ -7,8 +7,11 @@
 #include <sys/resource.h>
 
 // Pre-processor directives, controlled from g++ -D inputs
-//#define ENABLE_IMSHOW		0
+//#define ENABLE_IMSHOW		1
+#define ENABLE_IMAGE_RESIZE	1
+#define ENABLE_RGB2GRAY		1
 
+/********************************************************************/
 #define FRAME_SKIP_RATE		10
 #define MAX_INPUT_ARG		4 //Includes the name of the executable
 #define MAX_STRING_LEN		30
@@ -28,6 +31,7 @@ int main(int argn, char** argv)
 	char* log_file_loc;
 
 	Mat images;
+	Mat resized_im;
 	int count = 0;
 	std::ofstream out_fp;
 
@@ -39,13 +43,14 @@ int main(int argn, char** argv)
 
 	log_file_loc = (char*)malloc(sizeof(char) * MAX_STRING_LEN);
 	strcpy(log_file_loc, argv[2]);
-	out_fp.open(log_file_loc, std::ofstream::app);
+	out_fp.open(log_file_loc, std::ofstream::app); //use app for appending data
 
 	frame_skip_rate = atoi(argv[3]);
 	vid_file_name = (char*)malloc(sizeof(char) * MAX_STRING_LEN);
 
 	strcpy(vid_file_name, argv[1]);
-	out_fp << ", Video file = " << vid_file_name << "Frame skip rate = " << frame_skip_rate << std::endl;
+	out_fp << "Video file = " << vid_file_name << ", Frame skip rate = " << frame_skip_rate << std::endl;
+	std::cout << "Frame skip rate = " << frame_skip_rate << std::endl;
 	
 	start_time = get_process_time();
 
@@ -64,13 +69,25 @@ int main(int argn, char** argv)
 			break;
 		}	
 		else
-		{			
+		{	
 			if(count > frame_skip_rate)
 			{
 				count = 0;
-				#ifdef ENABLE_IMSHOW
+				#ifdef ENABLE_RGB2GRAY
+				cvtColor(images, images, CV_RGB2GRAY);
+				#endif
+
+				#ifdef ENABLE_IMAGE_RESIZE
+					resize(images, resized_im, Size(800,600));		
+					#ifdef ENABLE_IMSHOW
+					imshow("Resized images",resized_im);
+					waitKey(5);
+					#endif
+				#else
+					#ifdef ENABLE_IMSHOW
 					imshow("Captured images",images);
-					if(waitKey(30)>=0) break;
+					waitKey(5);
+					#endif
 				#endif
 			}
 		}
