8a6b9edd3e32481eb7c20b38086569568588ea54
rohitkum <rohitkum@umich.edu>
2017-10-30 19:51:21 -0400

    Removing all frame skip rates and only working with consecutive frames

--- sys_perf/captureVid.cpp
+++ sys_perf/captureVid.cpp
@@ -26,28 +26,24 @@ double get_process_time(void)
 	return (double(std::clock()) * 1000/CLOCKS_PER_SEC);
 }
 #else
-//void get_process_time(struct timeval *t) 
-//{
-//	gettimeofday(t, NULL);
-//}
+
 #endif
 
 int main(int argn, char** argv)
 {
 	#ifdef TIME_FROM_CLOCK 
-	double start_time, vid_load_end_time, end_time;
+	double start_time, time_1, time_2, time_3, time_4, time_5, time_6, end_time;
 	#else
-	struct timeval start_time, vid_load_end_time, end_time;
+	struct timeval start_time, time_1, time_2, time_3, time_4, time_5, time_6, end_time;
 	double elapsed_time = 0;
-	//clockid_t clk_id = CLOCK_MONOTONIC;
-	//struct timespec start_time, vid_load_end_time, end_time;
 	#endif
 
 	int frame_skip_rate = FRAME_SKIP_RATE;
 	char* vid_file_name;
 	char* log_file_loc;
 	Scalar tot_sum, max_sum;
-	
+	unsigned long long num_of_frames = 0;
+
 	Mat prev_image, next_image;
 	Mat prev_resized_im, next_resized_im;
 	Mat diff_image;
@@ -75,66 +71,97 @@ int main(int argn, char** argv)
 	#ifdef TIME_FROM_CLOCK	
 	start_time = get_process_time();
 	#else
-	//get_process_time(&start_time);
 	gettimeofday(&start_time, NULL);
-	//if(!clock_gettime(clk_id, &start_time))
-	//{
-	//	std::cout << "Clock cannot be recorded " << std::endl;
-	//}
-
 	#endif
 	VideoCapture cap(vid_file_name);
 	
 	#ifdef TIME_FROM_CLOCK
-	vid_load_end_time = get_process_time();
+	time_1 = get_process_time();
 	#else
-	//get_process_time(&end_time);
-	gettimeofday(&vid_load_end_time, NULL);
-	//if(!clock_gettime(clk_id, &vid_load_end_time))
-	//{
-	//	std::cout << "Clock cannot be recorded " << std::endl;
-	//}
+	gettimeofday(&time_1, NULL);
 	#endif
 	if(!cap.isOpened())
 	{
 		std::cout << "ERROR: Video cannot be opened\n";
 		return -1;
 	}
+	#ifdef TIME_FROM_CLOCK	
+	time_2 = get_process_time();
+	#else
+	gettimeofday(&time_2, NULL);
+	#endif
 	
-	if(!cap.read(prev_image)) // Read one image at the start
+	//if(!cap.read(prev_image)) // Read one image at the start
+	if(!cap.grab())
 	{
 		#ifdef TIME_FROM_CLOCK
 		end_time = get_process_time();
 		#else
-		//get_process_time(&end_time);
 		gettimeofday(&end_time, NULL);
-		//if(!clock_gettime(clk_id, &end_time))
-		//{
-		//	std::cout << "Clock cannot be recorded " << std::endl;
-		//}
 		#endif
 		cap.release();
-		std::cout << "ERROR: Not even one frame \n";
+		std::cout << "ERROR: Couldn't grab the first frame \n";
 		return -1;
-	}	
+	}
+
+	if(!cap.retrieve(prev_image, 0))
+	{
+		#ifdef TIME_FROM_CLOCK
+		end_time = get_process_time();
+		#else
+		gettimeofday(&end_time, NULL);
+		#endif
+		cap.release();
+		std::cout << "ERROR: Couldn't retrive the first frame \n";
+		return -1;
+
+	}
+	num_of_frames++;
+	
+	#ifdef TIME_FROM_CLOCK	
+	time_3 = get_process_time();
+	#else
+	gettimeofday(&time_3, NULL);
+	#endif
+	
 	cvtColor(prev_image, prev_image, CV_RGB2GRAY);
+	
+	#ifdef TIME_FROM_CLOCK	
+	time_4 = get_process_time();
+	#else
+	gettimeofday(&time_4, NULL);
+	#endif
 
 	#ifdef ENABLE_IMAGE_RESIZE
 	resize(prev_image, prev_resized_im, Size(800,600));
+	#endif
+	
+	#ifdef TIME_FROM_CLOCK	
+	time_5 = get_process_time();
+	#else
+	gettimeofday(&time_5, NULL);
 	#endif
 
 	for(;;)
 	{
 		count++;
-		if(!cap.read(next_image))
+		//if(!cap.read(next_image))
+		if(!cap.grab())
 		{
 			break;
 		}	
 		else
 		{	
-			if(count > frame_skip_rate)
+			if(count >= frame_skip_rate)
 			{
+				if(!cap.retrieve(next_image, 0))
+				{
+					break;
+				}
+				else
+				{
 				count = 0;
+				num_of_frames++;
 				#ifdef ENABLE_RGB2GRAY
 				cvtColor(next_image, next_image, CV_RGB2GRAY);
 				#endif
@@ -144,7 +171,7 @@ int main(int argn, char** argv)
 					diff_image = next_resized_im - prev_resized_im;
 					#ifdef ENABLE_IMSHOW
 					imshow("Resized images",diff_image);
-					waitKey(5);
+					waitKey(1);
 					#endif
 					tot_sum = sum(diff_image)/(800*600*255);
 					#if (ENABLE_DIFF_WRITE_FILE == 1)
@@ -155,7 +182,7 @@ int main(int argn, char** argv)
 					diff_image = next_image - prev_image;
 					#ifdef ENABLE_IMSHOW
 					imshow("Diff images",diff_image);
-					waitKey(5);
+					waitKey(1);
 					#endif
 					tot_sum = sum(diff_image)/(diff_image.cols*diff_image.rows*255);
 					#if (ENABLE_DIFF_WRITE_FILE == 1)
@@ -163,31 +190,39 @@ int main(int argn, char** argv)
 					#endif
 					prev_image = next_image.clone();
 				#endif
+				}
 			}
 		}
 	}
 	#ifdef TIME_FROM_CLOCK
 	end_time = get_process_time();
 	#else
-	//get_process_time(&end_time);
 	gettimeofday(&end_time, NULL);
-	//if(!clock_gettime(clk_id, &end_time))
-	//{
-	//	std::cout << "Clock cannot be recorded " << std::endl;
-	//}
 	#endif
 	cap.release();
 
 	#ifdef TIME_FROM_CLOCK
-	out_fp << "Elapsed time " << (end_time - start_time) << std::endl;
-	out_fp << "Video load time " << (vid_load_end_time - start_time) << std::endl;
+	out_fp << "Num of Frames = " << num_of_frames << std::endl;
+	out_fp << " Total time = " << (end_time - start_time) << ", Video Load = "<< (time_1 - start_time) << ", Video Open Check = " << (time_2 - time_1) << ", Im read = " << (time_3 - time_2) << ", RGB 2 Gray = " << (time_4 - time_3) << ", Resize = "<< (time_5 - time_4) << std::endl;
 	#else
+	out_fp << "Num of Frames = " << num_of_frames << std::endl;
 	elapsed_time = ((end_time.tv_sec - start_time.tv_sec) * 1000) + ((end_time.tv_usec - start_time.tv_usec)/1000); 
-	//elapsed_time = ((end_time.tv_sec - start_time.tv_sec) * 1000) + ((end_time.tv_sec - start_time.tv_sec)/1000); 
-	out_fp << "Elapsed time " << elapsed_time  << std::endl;
+	out_fp << "Total time = " << elapsed_time ;
 	
-	elapsed_time = ((vid_load_end_time.tv_sec - start_time.tv_sec) * 1000) + ((vid_load_end_time.tv_usec - start_time.tv_usec)/1000); 
-	out_fp << "Video load time " << elapsed_time  << std::endl;
+	elapsed_time = ((time_1.tv_sec - start_time.tv_sec) * 1000) + ((time_1.tv_usec - start_time.tv_usec)/1000); 
+	out_fp << ", Video load = " << elapsed_time ;
+	
+	elapsed_time = ((time_2.tv_sec - time_1.tv_sec) * 1000) + ((time_2.tv_usec - time_1.tv_usec)/1000); 
+	out_fp << ", Video Open Check = " << elapsed_time;
+	
+	elapsed_time = ((time_3.tv_sec - time_2.tv_sec) * 1000) + ((time_3.tv_usec - time_2.tv_usec)/1000); 
+	out_fp << ", Im read = " << elapsed_time;
+	
+	elapsed_time = ((time_4.tv_sec - time_3.tv_sec) * 1000) + ((time_4.tv_usec - time_3.tv_usec)/1000); 
+	out_fp << ", RGB 2 Gray = " << elapsed_time;
+	
+	elapsed_time = ((time_5.tv_sec - time_4.tv_sec) * 1000) + ((time_5.tv_usec - time_4.tv_usec)/1000); 
+	out_fp << ", Resize = " << elapsed_time  << std::endl;
 	#endif
 	out_fp.close();
 	return 0;
