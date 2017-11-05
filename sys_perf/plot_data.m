c34bbfed26f0595b83f39ebff6cc31aa4be3d8bb
rohithub <rohitkum@umich.edu>
2017-11-05 14:12:49 -0500

    Matlab Script: Plot the 6 different elapsed time for each log file. Added two PNG images generated of the script

--- sys_perf/plot_data.m
+++ sys_perf/plot_data.m
@@ -2,54 +2,39 @@ clear;
 close all;
 clc;
 
-num_frame_skips = 6; %[5 10 20 50 100 200]
-num_data_points = 1160; %assuming for frame_skip = 5
+%Log file format (all values in milliseconds)
+%Total time, Video Load, Open Chk, IM Read, RGB 2 GRAY, Resize, 
+%66913       275         0         79       15          3
 
-path = 'C:\Users\Rohit\Documents\GitHub\video-query\sys_perf\logs\log_no_imshow_cache_clr.txt';
+num_data_per_iter = 6; % Teel the number of data points collected for each iteration
+num_frame_skips = 0; %[5 10 20 50 100 200]
+%path = 'C:\Users\Rohit\Documents\GitHub\video-query\sys_perf\logs\log_no_imshow_cache_clr.txt';
+path = 'C:\Users\Rohit\Documents\GitHub\video-query\sys_perf\logs\log_no_imshow.txt';
 
+%Read the log file, scan to get each data point and close the file
 file_p = fopen(path,'r');
 myContent = textscan(file_p, '%s');
-data_matrix = zeros(num_frame_skips, num_data_points);
-
-i = 0;
-m = 0;
-count = 0;
-mode = 0;
-
-while(i < length(myContent{1,1})) 
-   i = i+1;
-   if(mode == 0)
-       if(strcmp(myContent{1,1}(i),'rate'))
-           mode = 1; % data collection phase
-           i = i+1; % shift one more as the next value is the frame rate value
-           m = m + 1;
-           count = 1; %reset the index of the array to write data
-       else 
-           continue;
-       end
-   else
-       if(strcmp(myContent{1,1}(i),'Elapsed'))
-           mode = 0; % scan phase
-           display(count);
-       else
-           data_matrix(m,count) = str2double(myContent{1,1}(i));
-           count = count + 1;
-       end
-   end
-end
 fclose(file_p);
 
-x_axis = zeros(1,num_data_points);
-for j=1:num_data_points;
-   x_axis(1,i) = 1;
+%Total number of iterations is equal to total data points/data points per
+%iteration
+tot_iters = length(myContent{1,1})/num_data_per_iter;
+data_matrix = zeros(num_data_per_iter, tot_iters);
+
+count = 1;
+for i=1:tot_iters
+    for j=1:num_data_per_iter
+        data_matrix(j,i) = str2double(myContent{1,1}(count));
+        count = count + 1;
+    end
 end
 
+plot_titles = {'Total time', 'Video Load', 'Check Opened', 'IM Read', 'RGB2Gray', 'Resize'};
 figure;
 for j=1:size(data_matrix,1)
     subplot(3,2,j);
     plot(data_matrix(j,2:end));
-    xlabel('Frame numbers');
-    ylabel('Total pixel sum/max sum');
-    myStr = sprintf('Frame Skip rate = %d',data_matrix(j,1));
-    title(myStr);
+    xlabel('Iteration #');
+    ylabel('Elapsed time');
+    title(plot_titles{1,j});
 end
