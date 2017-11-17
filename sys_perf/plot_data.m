371cec50ef3561b280e1c49b6e73d8c95145f521
rohithub <rohitkum@umich.edu>
2017-11-17 12:52:07 -0500

    Removed unneccessary Hist plot. Corrected Matlab code for summing over 5 iterations, not 38400. Also updated the Hist plot for 400x400 video

--- sys_perf/plot_data.m
+++ sys_perf/plot_data.m
@@ -7,11 +7,11 @@ clear; close all; clc;
 %Individual Img Proc log file format  (all values in microseconds)
 %Grab Time, Retrieve time, Color, Resize
 
-num_data_per_iter = 6; % Tells the number of data points collected for each iteration
+num_data_per_iter = 4; % Tells the number of data points collected for each iteration
 num_frame_skips = 0; %[5 10 20 50 100 200]
 num_of_iter = 5;
-path = 'C:\Users\Rohit\Documents\Umich Courses\Prof Wenisch Project\Logs\mod_log_no_imshow_all.txt';
-% path = 'C:\Users\Rohit\Documents\Umich Courses\Prof Wenisch Project\Logs\mod_log_no_imshow.txt';
+% path = 'C:\Users\Rohit\Documents\Umich Courses\Prof Wenisch Project\Logs\mod_log_no_imshow_all.txt';
+path = 'C:\Users\Rohit\Documents\Umich Courses\Prof Wenisch Project\Logs\mod_log_no_imshow.txt';
 %path = 'C:\Users\Rohit\Documents\GitHub\video-query\sys_perf\logs\log_no_imshow.txt';
 
 %Read the log file, scan to get each data point and close the file
@@ -22,21 +22,24 @@ fclose(file_p);
 %Total number of iterations is equal to total data points/data points per
 %iteration
 tot_iters = (length(myContent{1,1}))/num_data_per_iter;
-data_matrix = zeros(num_data_per_iter, tot_iters);
-sum_matrix = zeros(num_data_per_iter, tot_iters);
+data_matrix = zeros(num_data_per_iter, ceil(tot_iters/num_of_iter));
+sum_matrix = zeros(num_data_per_iter, ceil(tot_iters/num_of_iter));
 count = 1;
 for i=1:tot_iters
     for j=1:num_data_per_iter
-        data_matrix(j,i) = str2double(myContent{1,1}(count));
+        data_matrix(j,mod(i, 6959)+1) = str2double(myContent{1,1}(count));
         count = count + 1;
     end
+    
+    if(mod(i, 6959) == 0)
     sum_matrix = sum_matrix + data_matrix;
+    end
 end
 
 
 sum_matrix = sum_matrix./num_of_iter;
 % plot_titles = {'Total time', 'Video Load', 'Check Opened', 'IM Grab', 'IM Retrieve', 'RGB2Gray', 'Resize'};
-plot_titles = {'Total Elapsed','Video Load','IM Grab', 'IM Retrieve', 'RGB2Gray', 'Resize'};
+plot_titles = {'IM Grab', 'IM Retrieve', 'RGB2Gray', 'Resize'};
 figure;
 
 x = 1:(size(data_matrix,2)-1);
@@ -46,13 +49,16 @@ x = 1:(size(data_matrix,2)-1);
 % ylabel('Elapsed time (usec)');
 % title('Time taken for processing each frame of the input video');
 % legend('Grab time', 'Retrieve time', 'RGB to Gray time', 'Resize time 224x224');
+% hold on;
 for j=1:size(sum_matrix,1)
-    subplot(3,2,j);
-%     plot(data_matrix(j,2:end));
+    
+%     plot(sum_matrix(j,2:end),'+--');
 %     xlabel('Iteration #');
 %     ylabel('Elapsed time (uSec)');
+    
+    subplot(2,2,j);
     histogram(sum_matrix(j,1:end));
-    grid on;
+    
     if(j==1)
         xlabel('Time for 1 operation (msec)');
     else
@@ -60,4 +66,9 @@ for j=1:size(sum_matrix,1)
     end
     ylabel('Number of frames');
     title(plot_titles{1,j});
+    
+    grid on;
+
 end
+%legend('Grab time', 'Retrieve time', 'RGB2Gray', 'Resize');
+% hold off;
